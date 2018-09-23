882b47ac95eba46a3a6efb96f741abb4612493c6
JK <jkchen46033@yahoo.com>
2018-09-23 17:33:37 -0600

    oop.go

--- oop.go
+++ oop.go
@@ -1,82 +1,104 @@
+// This file demonstrates subclassing via embedded typing,
+// and polymorphism via interface
+
 package main
 
 import (
 	"fmt"
 )
 
-type employee struct {
-	name            string
-	vacationBalance int
-	bonus           float64
+type Introduce interface {
+	introduce()
 }
 
-type manager struct {
-	employee
+type CalculateBonus interface {
+	calculateBonus() float64
 }
 
-func NewEmployee(name string, vacationBalance int, bonus float64) *employee {
-	return &employee{name, vacationBalance, bonus}
+type Employee struct {
+	name   string
+	salary float64
+	bonus  float64
 }
 
-type Speak interface {
-	speak() string
+func NewEmployee(name string, salary float64, bonus float64) *Employee {
+	return &Employee{name, salary, bonus}
 }
 
-func (emp *employee) setNextYearBonus(rate float64) {
-	emp.bonus = emp.bonus * rate
+type Manager struct {
+	Employee
+	office string
 }
 
-func (emp *employee) speak() string {
-	return "ok"
+func (emp *Employee) calculateBonus() float64 {
+	emp.bonus = 2000 - emp.salary*0.01
+	return emp.bonus
 }
 
-func (mgr *manager) setNextYearBonus(rate float64) {
-	mgr.bonus = mgr.bonus * rate
+func (emp *Employee) introduce() {
+	for i := 0; i < 2; i++ {
+		fmt.Println("My name " + emp.name + ", and I am solid")
+	}
 }
 
-func (mgr *manager) speak() string {
-	return "cool down!"
-}
-func talk(head Speak) {
-	fmt.Println(head)
-	fmt.Println(head.speak())
+func NewManager(name string, salary float64, bonus float64, office string) *Manager {
+	return &Manager{Employee{name, salary, bonus}, office}
 }
 
-func totalBonus(list []*employee) float64 {
+func (mgr *Manager) calculateBonus() float64 {
+	mgr.bonus = 8000 + mgr.salary*0.10
+	return mgr.bonus
+}
+
+func (mgr *Manager) introduce() {
+	for i := 0; i < 3; i++ {
+		fmt.Println("Manager speaking here: My name "+mgr.name+", office is at", mgr.office)
+	}
+}
+
+func calculateTotalBonus(emps ...CalculateBonus) float64 {
+	var sum float64
+	for _, emp := range emps {
+		sum = sum + emp.calculateBonus()
+	}
+	return sum
+}
+
+func everyBodyIntroduce(emps ...Introduce) {
+	for _, emp := range emps {
+		emp.introduce()
+	}
+}
+
+func totalBonus(emps []*Employee) float64 {
 	var total float64
-	for _, emp := range list {
+	for _, emp := range emps {
 		total = total + emp.bonus
 	}
 	return total
 }
 
 func main() {
-	empA := NewEmployee("John L", 30, 20000.00)
-	fmt.Println(*empA)
-	mgrB := NewEmployee("Steve J", 55, 50000.00)
-	fmt.Println(*mgrB)
-	empA.setNextYearBonus(1.2)
-	mgrB.setNextYearBonus(1.5)
-	fmt.Println(*empA)
-	fmt.Println(*mgrB)
+	empA := NewEmployee("John L", 80000.00, 0)
+	mgrB := NewManager("Steve J", 150000.00, 0, "corner office E")
 
-	empList := make([]*employee, 0)
-	empList = append(empList, empA)
-	empList = append(empList, mgrB)
-	fmt.Println("Total bonus next year:", totalBonus(empList))
-	talk(empA)
-	talk(mgrB)
+	fmt.Println(empA)
+	fmt.Println(mgrB)
+
+	everyBodyIntroduce(empA, mgrB)
+
+	fmt.Printf("Total bonus is %f\n", calculateTotalBonus(empA, mgrB))
 }
 
 /*
 $ go run oop.go
-{John L 30 20000}
-{Steve J 55 50000}
-{John L 30 24000}
-{Steve J 55 75000}
-Total bonus next year: 99000
-&{John L 30 24000}
-ok
-&{Steve J 55 75000}
-ok
+&{John L 80000 0}
+&{{Steve J 150000 0} corner office E}
+My name John L, and I am solid
+My name John L, and I am solid
+Manager speaking here: My name Steve J, office is at corner office E
+Manager speaking here: My name Steve J, office is at corner office E
+Manager speaking here: My name Steve J, office is at corner office E
+Total bonus is 24200.000000
+
 */
