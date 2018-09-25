6603f2141016606245754a09104d2394f4066baf
JK <jkchen46033@yahoo.com>
2018-09-24 22:03:15 -0600

    map.go

--- map.go
+++ map.go
@@ -7,62 +7,61 @@ import (
 	"fmt"
 )
 
-type Golfer struct {
-	Fingerprint string
+type Person struct {
+	fingerprint string
 	name        string
 	val         []float64
 }
 
 type Node struct {
-	golfer *Golfer
+	person *Person
 	index  int
 }
 
 func main() {
-	// three golfers, two with same fingerprints.
-	golfer0 := &Golfer{"342482AD3F", "John Doe", []float64{48.0, 21.3, 120.5}}
-	golfer1 := &Golfer{"01234568ABCDE", "Donald Gate", []float64{0.0, 0.0, 1.0, 4.0}}
-	golfer2 := &Golfer{"342482AD3F", "Jacky Kim", []float64{27.2, 28.8, 250.4}}
+	// three persons, two with same fingerprints.
+	person0 := &Person{"342482AD3F", "John Doe", []float64{48.0, 21.3, 120.5}}
+	person1 := &Person{"01234568ABCDE", "Donald Gate", []float64{0.0, 0.0, 1.0, 4.0}}
+	person2 := &Person{"342482AD3F", "Jacky Kim", []float64{27.2, 28.8, 250.4}}
 
-	// golfers stored in a linear list
-	node0 := &Node{golfer0, 0}
-	node1 := &Node{golfer1, 1}
-	node2 := &Node{golfer2, 2}
+	// persons stored in a linear list
+	node0 := &Node{person0, 0}
+	node1 := &Node{person1, 1}
+	node2 := &Node{person2, 2}
 	slice := []*Node{node0, node1, node2}
 
-	// golfers also stored in a map; for fast retrieval by fingerprint
-	gmap := make(map[string][]*Node)
-	gmap["342482AD3F"] = append(gmap["342482AD3F"], node0, node2)
-	gmap["01234568ABCDE"] = append(gmap["01234568ABCDE"], node1)
+	// persons also stored in a map; for fast retrieval by fingerprint
+	pmap := make(map[string][]*Node)
+	pmap["342482AD3F"] = append(pmap["342482AD3F"], node0, node2)
+	pmap["01234568ABCDE"] = append(pmap["01234568ABCDE"], node1)
 
 	// print our linear list
-	fmt.Println("Golfers list:")
+	fmt.Println("Persons list:")
 	for _, node := range slice {
-		fmt.Printf("Golfer %d, %v\n", node.index, *node.golfer)
+		fmt.Printf("Person %d, %v\n", node.index, *node.person)
 	}
 
 	// retrieve by fingerprint
 	searchKeys := []string{"342482AD3F", "01234568ABCDE", "834A83BD"}
 	for _, searchKey := range searchKeys {
-		nodes := gmap[searchKey]
-		fmt.Printf("List of golfers with fingerprint %s, numbers: %d\n", searchKey, len(nodes))
+		nodes := pmap[searchKey]
+		fmt.Printf("List of persons with fingerprint %s, numbers: %d\n", searchKey, len(nodes))
 		for _, node := range nodes {
-			fmt.Printf("%v, %v\n", node.index, *node.golfer)
+			fmt.Printf("%v, %v\n", node.index, *node.person)
 		}
 	}
 }
 
 /*
 $ go run map.go
-Golfers list:
-Golfer 0, {342482AD3F John Doe [48 21.3 120.5]}
-Golfer 1, {01234568ABCDE Donald Gate [0 0 1 4]}
-Golfer 2, {342482AD3F Jacky Kim [27.2 28.8 250.4]}
-List of golfers with fingerprint 342482AD3F, numbers: 2
+Persons list:
+Person 0, {342482AD3F John Doe [48 21.3 120.5]}
+Person 1, {01234568ABCDE Donald Gate [0 0 1 4]}
+Person 2, {342482AD3F Jacky Kim [27.2 28.8 250.4]}
+List of persons with fingerprint 342482AD3F, numbers: 2
 0, {342482AD3F John Doe [48 21.3 120.5]}
 2, {342482AD3F Jacky Kim [27.2 28.8 250.4]}
-List of golfers with fingerprint 01234568ABCDE, numbers: 1
+List of persons with fingerprint 01234568ABCDE, numbers: 1
 1, {01234568ABCDE Donald Gate [0 0 1 4]}
-List of golfers with fingerprint 834A83BD, numbers: 0
-
+List of persons with fingerprint 834A83BD, numbers: 0
 */
