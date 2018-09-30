464e7a8d1138609a13baf827dc6071c047a84694
JK <jkchen46033@yahoo.com>
2018-09-30 14:57:15 -0600

    queue4.go

--- queue4.go
+++ queue4.go
@@ -1,5 +1,5 @@
 // This function implments a unlimited circular queue in golang
-// The idea is double the slze of slice when queue is full during push
+// The idea is to double the size of when queue detects full during push
 
 package main
 
@@ -14,8 +14,10 @@ type Queue struct {
 	size  int
 }
 
-func NewQueue() *Queue {
-	initSize := 3
+func NewQueue(initSize int) *Queue {
+	if initSize < 1 {
+		panic(fmt.Sprintf("NewQueue(%v), Oh please dont give me a value less than one\n", initSize))
+	}
 	return &Queue{make([]int, initSize, initSize), -1, -1, initSize}
 }
 
@@ -67,47 +69,40 @@ func (q *Queue) Copy() {
 	q.size = (newSize)
 	q.front = -1
 	q.tail = oldSize - 1
-	fmt.Printf("Copy() called, expand to size %d\n", newSize)
 }
 
 func main() {
-	q := NewQueue()
+	q := NewQueue(3)
 	fmt.Printf("%+v\n", q)
 	fmt.Printf("IsFull? %v, IsEmpty? %v, Length %d,  %+v\n", q.IsFull(), q.IsEmpty(), q.Length(), q)
 	q.Push(1)
 	q.Push(2)
+	fmt.Printf("Pushed 1,2, IsFull? %v, IsEmpty? %v, Length %d,  %+v\n", q.IsFull(), q.IsEmpty(), q.Length(), q)
 	q.Push(3)
 	q.Push(4)
-	fmt.Printf("%+v\n", q)
-	fmt.Printf("IsFull? %v, IsEmpty? %v, Length %d,  %+v\n", q.IsFull(), q.IsEmpty(), q.Length(), q)
+	fmt.Printf("Pushed 3,4, IsFull? %v, IsEmpty? %v, Length %d,  %+v\n", q.IsFull(), q.IsEmpty(), q.Length(), q)
 
-	fmt.Println(q.Pop())
-	fmt.Println(q.Pop())
-	fmt.Println(q.Pop())
-	fmt.Println(q.Pop())
+	fmt.Printf("Poped %d\n", q.Pop())
+	fmt.Printf("Poped %d\n", q.Pop())
+	fmt.Printf("Poped %d\n", q.Pop())
+	fmt.Printf("Poped %d\n", q.Pop())
 	fmt.Printf("IsFull? %v, IsEmpty? %v, Length %d,  %+v\n", q.IsFull(), q.IsEmpty(), q.Length(), q)
 	q.Push(5)
-	fmt.Printf("IsFull? %v, IsEmpty? %v, Length %d,  %+v\n", q.IsFull(), q.IsEmpty(), q.Length(), q)
-	fmt.Println(q.Pop())
-	fmt.Printf("IsFull? %v, IsEmpty? %v, Length %d,  %+v\n", q.IsFull(), q.IsEmpty(), q.Length(), q)
 	q.Push(6)
-	fmt.Printf("IsFull? %v, IsEmpty? %v, Length %d,  %+v\n", q.IsFull(), q.IsEmpty(), q.Length(), q)
+	fmt.Printf("Pushed 5, 6, IsFull? %v, IsEmpty? %v, Length %d,  %+v\n", q.IsFull(), q.IsEmpty(), q.Length(), q)
 }
 
 /*
-g$ go run queue4.go
+$ go run queue4.go
 &{q:[0 0 0] front:-1 tail:-1 size:3}
 IsFull? false, IsEmpty? true, Length 0,  &{q:[0 0 0] front:-1 tail:-1 size:3}
-Copy() called, expand to size 6
-&{q:[1 2 3 4 0 0] front:-1 tail:3 size:6}
-IsFull? false, IsEmpty? false, Length 4,  &{q:[1 2 3 4 0 0] front:-1 tail:3 size:6}
-1
-2
-3
-4
+Pushed 1,2, IsFull? false, IsEmpty? false, Length 2,  &{q:[1 2 0] front:-1 tail:1 size:3}
+Pushed 3,4, IsFull? false, IsEmpty? false, Length 4,  &{q:[1 2 3 4 0 0] front:-1 tail:3 size:6}
+Poped 1
+Poped 2
+Poped 3
+Poped 4
 IsFull? false, IsEmpty? true, Length 0,  &{q:[1 2 3 4 0 0] front:3 tail:3 size:6}
-IsFull? false, IsEmpty? false, Length 1,  &{q:[1 2 3 4 5 0] front:3 tail:4 size:6}
-5
-IsFull? false, IsEmpty? true, Length 0,  &{q:[1 2 3 4 5 0] front:4 tail:4 size:6}
-IsFull? false, IsEmpty? false, Length 1,  &{q:[1 2 3 4 5 6] front:4 tail:5 size:6}
+Pushed 5, 6, IsFull? false, IsEmpty? false, Length 2,  &{q:[1 2 3 4 5 6] front:3 tail:5 size:6}
+
 */
