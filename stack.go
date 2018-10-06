c1ba6c082e7a8e13df18d2dd84fe6fd806b18520
JK <jkchen46033@yahoo.com>
2018-10-06 11:10:05 -0600

    stack.go

--- stack.go
+++ stack.go
@@ -12,23 +12,22 @@ func NewStack() *Stack {
 	return &Stack{make([]int, 0)}
 }
 
-func (s *Stack) Push(val int) {
-	s.stack = append(s.stack, val)
+func (s *Stack) Push(val ...int) {
+	s.stack = append(s.stack, val...)
 }
 
 func (s *Stack) Pop() int {
-	val := s.stack[len(s.stack)-1]
-	s.stack = s.stack[0 : len(s.stack)-1]
+	length := len(s.stack)
+	val := s.stack[length-1]
+	s.stack = s.stack[0 : length-1]
 	return val
 }
 
 func main() {
 	stack := NewStack()
 
-	// push 1 2 3 to stack
 	stack.Push(1)
-	stack.Push(2)
-	stack.Push(3)
+	stack.Push(2, 3)
 	fmt.Println("pushed 1, 2, 3, to stack, ", *stack)
 	// pop
 	fmt.Println("poped from stack, ", stack.Pop())
@@ -37,6 +36,7 @@ func main() {
 	// the remaing stack
 	fmt.Println("stack:", *stack)
 }
+
 /*
 $ go run stack.go
 pushed 1, 2, 3, to stack,  {[1 2 3]}
