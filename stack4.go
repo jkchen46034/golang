ff987e9d22193d1cbaed463571dced67c351aa97
JK <jkchen46033@yahoo.com>
2018-10-06 12:55:09 -0600

    thread stack

--- stack4.go
+++ stack4.go
@@ -1,48 +1,74 @@
 // Implements a thread safe stack
+
 package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 type Stack struct {
+	mutex sync.Mutex
 	stack []int
 }
 
 func NewStack() *Stack {
-	return &Stack{make([]int, 0)}
+	return &Stack{stack: make([]int, 0)}
 }
 
 func (s *Stack) Push(val ...int) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	s.stack = append(s.stack, val...)
 }
 
 func (s *Stack) Pop() int {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	length := len(s.stack)
 	val := s.stack[length-1]
 	s.stack = s.stack[0 : length-1]
 	return val
 }
 
-func main() {
-	stack := NewStack()
+func (s *Stack) thread(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < 200000; i++ {
+		s.Push(1, 2, 3, 4, 5)
+		s.Pop()
+		s.Pop()
+		s.Pop()
+		s.Pop()
+		s.Pop()
+	}
+	fmt.Println("done")
+}
 
-	stack.Push(1)
-	stack.Push(2, 3)
-	fmt.Println("pushed 1, 2, 3, to stack, ", *stack)
-	// pop
-	fmt.Println("poped from stack, ", stack.Pop())
-	// pop
-	fmt.Println("poped from stack, ", stack.Pop())
-	// the remaing stack
-	fmt.Println("stack:", *stack)
+func main() {
+	var wg sync.WaitGroup
+	s := NewStack()
+	s.Push(8, 9, 10, 11, 12, 13)
+	fmt.Println(s.stack)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go s.thread(&wg)
+	}
+	wg.Wait()
+	fmt.Println(s.stack)
 }
 
 /*
-$ go run stack.go
-pushed 1, 2, 3, to stack,  {[1 2 3]}
-poped from stack,  3
-poped from stack,  2
-stack: {[1]}
-
+$ go run stack4.go
+[8 9 10 11 12 13]
+done
+done
+done
+done
+done
+done
+done
+done
+done
+done
+[8 9 10 11 12 13]
 */
