34d5dbd6930f3820b762e104115bb9ca320df8b2
JK <jkchen46033@yahoo.com>
2018-10-07 18:39:50 -0600

    blockchain

--- blockchain.go
+++ blockchain.go
@@ -37,7 +37,7 @@ func NewBlock(data string, Prehash []byte) *Block {
 }
 
 func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Bllock", []byte{})
+	return NewBlock("Genesis Block", []byte{})
 }
 
 func NewBlockChain() *BlockChain {
@@ -52,7 +52,7 @@ func (chain *BlockChain) AddBlock(data string) {
 
 func print(chain *BlockChain) {
 	for _, b := range chain.Blocks {
-		fmt.Printf("%+v %+v %+v\n", b.Data, b.Prehash, b.Hash)
+		fmt.Printf("Data: %s\nPrevious Hash: %X\nHash: %X\n", b.Data, b.Prehash, b.Hash)
 	}
 }
 
@@ -60,15 +60,18 @@ func main() {
 	bc := NewBlockChain()
 	bc.AddBlock("sent to J 100 million dollars")
 	bc.AddBlock("charged J 110 million dollars")
-	fmt.Printf("%+v\n", bc)
 	print(bc)
 }
 
 /*
 $ go run blockchain.go
-&{Blocks:[0xc000084000 0xc000084050 0xc0000840a0]}
-[71 101 110 101 115 105 115 32 66 108 108 111 99 107] [] [0 215 243 145 92 198 227 22 73 210 144 150 189 156 70 117 60 125 15 169 0 237 153 227 140 37 14 95 228 80 35 112]
-[115 101 110 116 32 116 111 32 74 32 49 48 48 32 109 105 108 108 105 111 110 32 100 111 108 108 97 114 115] [0 215 243 145 92 198 227 22 73 210 144 150 189 156 70 117 60 125 15 169 0 237 153 227 140 37 14 95 228 80 35 112] [159 247 170 168 243 101 34 38 146 40 81 102 42 222 53 186 200 251 101 136 227 155 174 140 212 241 41 252 99 114 23 48]
-[99 104 97 114 103 101 100 32 74 32 49 49 48 32 109 105 108 108 105 111 110 32 100 111 108 108 97 114 115] [159 247 170 168 243 101 34 38 146 40 81 102 42 222 53 186 200 251 101 136 227 155 174 140 212 241 41 252 99 114 23 48] [101 55 183 52 117 229 27 37 228 151 50 28 2 137 61 83 150 244 154 234 228 207 146 6 251 115 171 172 209 203 154 99]
-
+Data: Genesis Block
+Previous Hash:
+Hash: 4432A4F78996CA511EB7F7ADD35D61D066558AD0D539B44213A37E266D571A67
+Data: sent to J 100 million dollars
+Previous Hash: 4432A4F78996CA511EB7F7ADD35D61D066558AD0D539B44213A37E266D571A67
+Hash: 7DA2128852033C5D7EEA445022CE9EF9274750C889C2DDFBB1B41B8ECB46FA13
+Data: charged J 110 million dollars
+Previous Hash: 7DA2128852033C5D7EEA445022CE9EF9274750C889C2DDFBB1B41B8ECB46FA13
+Hash: B6BB6E0110CD9F61CF1684922EAF3390F429974EFE29B586FE7B3F5FAD486A69
 */
