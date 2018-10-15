24021203d5aaf0d410e29931d1c0477f7dc35afe
JK Chen <jkchen46033@yahoo.com>
2018-10-14 18:09:45 -0600

    simple web service

---
+++ wikipage/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func loadPage(filename string) (*Page, error) {
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+	return &Page{filename, body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := string(r.URL.Path[len("/view/"):]) + ".txt"
+	page, err := loadPage(title)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+	fmt.Fprintf(w, string(page.Body))
+}
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	log.Println("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
