9345f342731fabc21e0231c06deef3b6071dc3a3
JK Chen <jkchen46033@yahoo.com>
2018-10-27 14:29:11 -0600

    concurrency pattern

--- concurrency/token.go
+++ concurrency/token.go
@@ -1,16 +1,22 @@
 // Implement a token pattern, sequencing a series of tasks
+// task implemented with generation pattern
 package main
 
 import (
 	"fmt"
 )
 
-func performTask(in chan int, out chan int) {
-	val := <-in
-	// do something ....
-	val = val + 3
-	//...
-	out <- val
+func performTask(in chan int) chan int {
+	out := make(chan int)
+	go func() {
+		val := <-in
+		// do something ....
+		val = val + 3
+		//...
+		out <- val
+		return
+	}()
+	return out
 }
 
 func main() {
@@ -19,14 +25,12 @@ func main() {
 	out := in
 	// build the work chain
 	for i := 0; i < 100; i++ {
-		out = make(chan int)
-		go performTask(in, out)
+		out = performTask(in)
 		in = out
 	}
 	// now trigger the work
 	begin <- 0
-	val := <-out
-	fmt.Println(val)
+	fmt.Println(<-out)
 }
 
 /*
