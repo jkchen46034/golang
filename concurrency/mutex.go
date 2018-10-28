6359823e5b60d4a0b296d54064d5f587b088357b
JK Chen <jkchen46033@yahoo.com>
2018-10-28 13:41:13 -0600

    mutex.go

--- concurrency/mutex.go
+++ concurrency/mutex.go
@@ -6,15 +6,9 @@ import (
 	"time"
 )
 
-func Adder1(a *int, m chan int) {
+func Adder(a *int, amount int, m chan int) {
 	<-m
-	*a = *a + 1
-	m <- 1
-}
-
-func Adder2(a *int, m chan int) {
-	<-m
-	*a = *a + 2
+	*a = *a + amount
 	m <- 1
 }
 
@@ -22,8 +16,8 @@ func main() {
 	m := make(chan int)
 	a := 0
 	for i := 0; i < 1000; i++ {
-		go Adder1(&a, m)
-		go Adder2(&a, m)
+		go Adder(&a, 1, m)
+		go Adder(&a, 2, m)
 	}
 	m <- 1
 	time.Sleep(10 * time.Millisecond)
