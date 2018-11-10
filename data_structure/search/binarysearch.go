2ecb5da6210f680dcc0749aff40386776a7f32cd
JK <jkc@trugolf.com>
2018-11-10 15:02:31 -0700

    binary search

--- data_structure/search/binarysearch.go
+++ data_structure/search/binarysearch.go
@@ -4,41 +4,40 @@ import (
 	"fmt"
 )
 
-func binarySearch(a []int, v int) bool {
-	n := len(a)
-	if n == 0 {
-		return false
-	}
-	if n == 1 {
-		if a[0] == v {
+func binarySearch(a []int, v int, r int, l int) bool {
+	if l > r {
+		mid := (l + r) / 2
+		if v == a[mid] {
 			return true
+		} else if v > a[mid] {
+			return binarySearch(a, v, mid+1, l)
+		} else {
+			return binarySearch(a, v, r, mid)
 		}
-		return false
 	}
-
-	mid := n / 2
-
-	found := binarySearch(a[0:mid], v)
-	if found {
-		return true
-	}
-	return binarySearch(a[mid:n], v)
+	return false
 }
 
 func main() {
 	a := []int{2, 3, 4, 7, 9, 12, 15, 28}
 	fmt.Println(a)
-	fmt.Println("Trying to find 9", binarySearch(a, 9))
-	fmt.Println("Trying to find 28", binarySearch(a, 28))
-	fmt.Println("Trying to find 4", binarySearch(a, 4))
-	fmt.Println("Trying to find 5", binarySearch(a, 5))
+	fmt.Println("Trying to find 9,", binarySearch(a, 9, 0, len(a)))
+	fmt.Println("Trying to find 28,", binarySearch(a, 28, 0, len(a)))
+	fmt.Println("Trying to find 4,", binarySearch(a, 4, 0, len(a)))
+	fmt.Println("Trying to find 5,", binarySearch(a, 5, 0, len(a)))
+	fmt.Println("Trying to find 13,", binarySearch(a, 13, 0, len(a)))
+	fmt.Println("Trying to find 0,", binarySearch(a, 0, 0, len(a)))
+	fmt.Println("Trying to find 100,", binarySearch(a, 100, 0, len(a)))
 }
 
 /*
 $ go run binarysearch.go
 [2 3 4 7 9 12 15 28]
-Trying to find 9 true
-Trying to find 28 true
-Trying to find 4 true
-Trying to find 5 false
+Trying to find 9, true
+Trying to find 28, true
+Trying to find 4, true
+Trying to find 5, false
+Trying to find 13, false
+Trying to find 0, false
+Trying to find 100, false
 */
