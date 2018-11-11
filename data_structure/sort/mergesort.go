3da464602927b6d4e971356f81e84c79ef8f30e2
JK Chen <jkchen46033@yahoo.com>
2018-11-10 23:52:09 -0700

    merge sort

--- data_structure/sort/mergesort.go
+++ data_structure/sort/mergesort.go
@@ -4,45 +4,37 @@ import (
 	"fmt"
 )
 
-func mergeSort(a []int) []int {
+func merge(a []int) {
 	n := len(a)
 	if n <= 1 {
-		return a
+		return
 	}
-	mid := int(float32(n)/2 + 0.5)
-	b := a[0:mid]
-	c := a[mid:n]
-
-	mergeSort(b)
-	mergeSort(c)
-	merge(b, c)
-	return a
+	m := n / 2
+	merge(a[0:m])
+	merge(a[m:n])
+	combine(a, m)
 }
 
-func merge(a []int, b []int) {
-	m := len(b)
-	n := len(a)
-	for i := 0; i < m; i++ {
-		target := b[i]
-		var j int
-		for j = 0; j < n && b[i] > a[j]; j++ {
+func combine(a []int, m int) {
+	for i := m; i < len(a); i++ {
+		k := i - 1
+		val := a[i]
+		for ; k >= 0 && a[k] > val; k-- {
+			a[k+1] = a[k]
 		}
-
-		a = append(a, 0)
-		n = n + 1
-		copy(a[j+1:n], a[j:n-1])
-		a[j] = target
+		a[k+1] = val
 	}
 }
 
 func main() {
 	a := []int{15, 2, 7, 12, 28, 3, 9, 4, 8}
 	fmt.Println(a)
-	fmt.Println(mergeSort(a))
+	merge(a)
+	fmt.Println(a)
 }
 
 /*
-$ go run mergesort.go
+$ go run merge.go
 [15 2 7 12 28 3 9 4 8]
 [2 3 4 7 8 9 12 15 28]
 */
