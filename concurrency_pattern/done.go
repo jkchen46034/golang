989bcdbafdb1b699f0ffb059d8b26332b5716799
JK <jkc@trugolf.com>
2018-12-06 12:27:07 -0700

    done channel

--- concurrency_pattern/done.go
+++ concurrency_pattern/done.go
@@ -1,74 +1,33 @@
-// done
+// done channel
 
 package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func multiplex(a chan int, b chan int, done chan int) chan int {
+func runserver() chan int {
 	c := make(chan int)
 	go func() {
-		for n := 2; n > 0; {
-			select {
-			case val := <-a:
-				c <- val
-			case val := <-b:
-				c <- val
-			case <-done:
-				n--
-			}
+		for i := 0; i < 3; i++ {
+			fmt.Println("server is running")
 		}
-		close(c)
+		c <- 1
 	}()
 
-	return c
-}
-
-func findPrime(from int, to int, done chan int) chan int {
-	c := make(chan int)
-	go func() {
-		for i := from; i < to; i++ {
-			if i < 2 {
-				continue
-			}
-			mid := int(math.Sqrt(float64(i)))
-			isPrime := true
-			for j := 2; j <= mid; j++ {
-				if i%j == 0 {
-					isPrime = false
-					break
-				}
-			}
-			if isPrime {
-				c <- i
-			}
-		}
-		done <- 1
-	}()
 	return c
 }
 
 func main() {
-	done := make(chan int)
-	a := findPrime(1, 10, done)
-	b := findPrime(10, 20, done)
-	c := multiplex(a, b, done)
-
-	for val := range c {
-		fmt.Println(val)
-	}
+	c := runserver()
+	<-c
+	fmt.Println("exited from main")
 }
 
 /*
 $ go run done.go
-2
-3
-5
-11
-7
-13
-17
-19
+server is running
+server is running
+server is running
+exited from main
 */
