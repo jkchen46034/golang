d20ee5ced4658e0aad6ea4c248cac080288169dd
jkchen46034 <jkc@trugolf.com>
2020-08-23 12:57:03 -0400

    binary search

--- data_structure/search/binarysearch.go
+++ data_structure/search/binarysearch.go
@@ -4,37 +4,27 @@ import (
 	"fmt"
 )
 
-func binarySearch(a []int, v int, left, right int) bool {
+func binarySearch(a []int, v int, left, right int) (bool, int) {
 	for left <= right {
 		mid := (left + right) / 2
 		if v == a[mid] {
-			return true
+			return true, mid
 		} else if v < a[mid] {
 			right = mid - 1
 		} else {
 			left = mid + 1
 		}
 	}
-	return false
+	return false, -1
 }
 
 func main() {
 	a := []int{2, 3, 4, 7, 9, 12, 15, 28}
 	fmt.Println(a)
-	fmt.Println("Trying to find 4", binarySearch(a, 4, 0, len(a) - 1))
-	fmt.Println("Trying to find 15", binarySearch(a, 15, 0, len(a) - 1))
-	fmt.Println("Trying to find 28", binarySearch(a, 28, 0, len(a) - 1))
-	fmt.Println("Trying to find 2", binarySearch(a, 2, 0, len(a) - 1))
-	fmt.Println("Trying to find 8", binarySearch(a, 8, 0, len(a) - 1))
-	fmt.Println("Trying to find 30", binarySearch(a, 30, 0, len(a) - 1))
+	exist, index := binarySearch(a, 4, 0, len(a) - 1)
+	fmt.Println("Trying to find 4: ", exist, index)
+	exist, index = binarySearch(a, 28, 0, len(a) - 1)
+	fmt.Println("Trying to find 28: ", exist, index)
+	exist, index = binarySearch(a, 0, 0, len(a) - 1)
+	fmt.Println("Trying to find 0: ", exist, index)
 }
-
-/*
-$ go run binarysearch.go
-[2 3 4 7 9 12 15 28]
-Trying to find 4 true
-Trying to find 9 true
-Trying to find 28 true
-Trying to find 8 false
-Trying to find 17 false
-*/
